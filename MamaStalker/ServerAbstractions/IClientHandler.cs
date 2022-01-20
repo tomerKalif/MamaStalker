5bcf6faa72d333454044e1fd7ca5743115ea255d
tomerKalif <tomer.kalif2003@gmail.com>
2022-01-20 12:48:07 +0200

    added ClientHandler

---
+++ MamaStalker/ServerAbstractions/IClientHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerAbstractions
+{
+    public interface IClientHandler
+    {
+        public Task SendToClient();
+
+        public Task ReciveFromClient();
+
+        public Task RunClient();
+    }
+}
